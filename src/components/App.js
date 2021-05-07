a3094232574d6678d2828302dbd24badd50d74b2
Crystal Joyce <crystaljoyce@me.com>
2021-05-06 21:15:17 -0700

    setting up home page

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'; 
+import { App } from '.';
+
+
+const App = () => { 
+
+
+    return ( <> 
+    
+    <div> 
+        <nav> 
+            <Link to="/"> Home</Link>
+        </nav>
+    </div>
+    </> )
+}
+
+export default App; 
